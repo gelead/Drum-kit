09ddc41d84f20fa9100e1a68aad410ec024d52e1
Gelead <geleadw@gmail.com>
2025-04-24 17:40:20 +0300

    Sound integration

--- src/Drum.jsx
+++ src/Drum.jsx
@@ -3,24 +3,33 @@ import initpads from "./pads";
 
 const Drum = ({ darkmode }) => {
   const [pads, setPads] = React.useState(initpads);
+  const [activePad, setActivePad] = React.useState(null);
 
-  const handleToggle = (id) => {
-    const updatedPads = pads.map((pad) =>
-      pad.id === id ? { ...pad, on: !pad.on } : pad
-    );
-    setPads(updatedPads);
+  const handleToggle = (id, sound) => {
+    const audio = new Audio(sound);
+    audio.play();
+    setActivePad(id); 
+
+    setTimeout(() => {
+      setActivePad(null);
+    }, 150);
   };
 
   return (
-    <div className={`grid grid-cols-4 gap-4 p-4 items-center ${darkmode ? "bg-black" : "bg-white"}`}>
+    <div
+      className={`p-8 grid grid-cols-4 gap-6 ${
+        darkmode ? "bg-zinc-900" : "bg-white"
+      }`}
+    >
       {pads.map((pad) => (
         <button
           key={pad.id}
-          onClick={() => handleToggle(pad.id)} 
+          onClick={() => handleToggle(pad.id, pad.sound)}
           style={{ backgroundColor: pad.color }}
-          className={`aspect-square rounded-lg transition-all duration-300 ${pad.on ? "" : "opacity-20"}`}
+          className={`w-full aspect-square rounded-2xl text-2xl font-bold shadow-lg hover:scale-105 transform transition-all duration-100 
+          ${activePad === pad.id ? "ring-3 ring-white" : ""}`}
         >
-          {pad.id}
+          Pad {pad.id}
         </button>
       ))}
     </div>
--- src/pads.jsx
+++ src/pads.jsx
@@ -1,43 +1,42 @@
-
 export default [
   {
-      id: 1,
-      color: "#F18D8B",
-      on: true
-  },   
+    id: 1,
+    color: "#F18D8B",
+    sound: "/sounds/sound1.wav",
+  },
   {
-      id: 2,
-      color: "#F5C280",
-      on: false
-  },   
+    id: 2,
+    color: "#F5C280",
+    sound: "/sounds/sound2.wav",
+  },
   {
-      id: 3,
-      color: "#EEEC79",
-      on: true
-  },   
+    id: 3,
+    color: "#EEEC79",
+    sound: "/sounds/sound3.wav",
+  },
   {
-      id: 4,
-      color: "#64ED98",
-      on: true
-  },   
+    id: 4,
+    color: "#64ED98",
+    sound: "/sounds/sound4.wav",
+  },
   {
-      id: 5,
-      color: "#63DEED",
-      on: false
-  },   
+    id: 5,
+    color: "#63DEED",
+    sound: "/sounds/sound7.wav",
+  },
   {
-      id: 6,
-      color: "#877FED",
-      on: false
-  },   
+    id: 6,
+    color: "#877FED",
+    sound: "/sounds/sound8.wav",
+  },
   {
-      id: 7,
-      color: "#A57FE9",
-      on: false
-  },   
+    id: 7,
+    color: "#A57FE9",
+    sound: "/sounds/sound9.wav",
+  },
   {
-      id: 8,
-      color: "#F289C1",
-      on: true
-  },   
-]
+    id: 8,
+    color: "#F289C1",
+    sound: "/sounds/sound10.wav",
+  },
+];
